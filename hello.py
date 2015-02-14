8fe937b4a54cb472d1ed133f0b074e21af5291c1
kyle billings <the.kyle.billings@gmail.com>
2015-02-14 01:37:42 -0800

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+import flask
+app = flask.Flask(__name__)
+
+@app.route('/<name>')
+def main(name=None):
+  return flask.render_template('main.html', name=name.title())
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    error = None
+    if flask.request.method == 'POST':
+       return flask.redirect(flask.url_for("main", name=flask.request.form["name"])) 
+    return flask.render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
